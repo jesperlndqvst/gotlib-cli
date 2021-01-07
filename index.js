434f55302c77d491ab3ff307b057fcd3141ed91a
Jesper <jesper.lundqvist@gmail.com>
2021-01-07 18:42:19 +0100

    Created prompt for choosing book

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import { waitForAndType } from './functions/waitForAndType.js';
     'https://www.gotlib.goteborg.se/iii/cas/login?service=https%3A%2F%2Fencore.gotlib.goteborg.se%3A443%2Fiii%2Fencore%2Fj_acegi_cas_security_check&lang=swe&suite=pearl'
   );
 
-  // ENTER USERNAME AND PASSWORD
+  console.log('LOGGING IN TO GOTLIB...');
   await waitForAndType(
     page,
     'input#code.loginField',
@@ -33,42 +33,66 @@ import { waitForAndType } from './functions/waitForAndType.js';
   });
   await waitForAndType(page, 'input#searchString', response.name);
   await page.keyboard.press('Enter');
+
+  console.log('SEARCHING FOR BOOK...');
   await page.waitForNavigation();
 
   // GET SEARCH RESULT
+  const getChoosenBook = async () => {
+    let isSure = false;
 
-  // await page.waitForSelector('.title');
-  const books = await page.$$eval('.title', (elements) => {
-    return elements.map((element, index) => {
-      const book = element.textContent
-        .trim()
-        .split('/ ')
-        .filter((item) => item !== '' && item !== null);
-      const title = book[0];
-      // const author = book[1];
-      return title;
-      return {
-        title,
-        author,
-        value: index,
-        // desc: desc,
-      };
-    });
-  });
+    while (!isSure) {
+      const titles = await page.$$eval('.dpBibTitle', (elements) =>
+        elements.map((element) => element.children[0].textContent.trim())
+      );
 
-  console.log(books);
+      const auhtors = await page.$$eval('.dpBibAuthor', (auhtor) =>
+        auhtor.map((auhtor) => auhtor.textContent.trim())
+      );
 
-  // const { character } = await prompts({
-  //   type: 'select',
-  //   name: 'character',
-  //   message: 'Pick a book',
-  //   choices: characters,
-  // });
+      const years = await page.$$eval('.itemMediaYear', (year) =>
+        year.map((year) => year.textContent.trim())
+      );
 
-  // console.log(characters[character]);
+      const availabilityMessages = await page.$$eval(
+        '.availabilityMessage',
+        (message) => message.map((message) => message.textContent.trim())
+      );
 
-  await page.screenshot({ path: 'example.png' });
-  console.log('SUCCESS! TAKE A LOOK AT THE SCREENSHOT');
+      const books = titles.map((title, i) => {
+        return {
+          title,
+          description: `  ${auhtors[i]} ${years[i]}
+        ${availabilityMessages[i]}`,
+          id: i,
+        };
+      });
+
+      const { book } = await prompts({
+        type: 'select',
+        name: 'book',
+        message: 'Pick a book',
+        choices: books,
+      });
+
+      const { value } = await prompts({
+        type: 'toggle',
+        name: 'value',
+        message: 'Are you sure you want to reserve this book?',
+        initial: true,
+        active: 'yes',
+        inactive: 'no',
+      });
+
+      if (value) {
+        isSure = true;
+        return books.find((item) => item.id === book);
+      }
+    }
+  };
+
+  const choosenBook = await getChoosenBook();
+  console.log(`You have reserved ${choosenBook.title}`);
 
   // CLOSE BROWSER
   await browser.close();
